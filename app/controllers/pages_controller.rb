511b39428c529fbd058e02f743bacccac2a33a3c
Stefan Wienert <stefan.wienert@pludoni.de>
2014-06-03 14:50:53 +0200

    sorting/year displaying

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def index
     @running = Challenge.running.visible.sorted
-    @upcoming = Challenge.upcoming.visible.sorted
+    @upcoming = Challenge.upcoming.visible.order('from_date asc')
     @archive = Challenge.visible.sorted.where('to_date < ?', Date.today)
   end
 
