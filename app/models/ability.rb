0ddee5832a9e2caa0d2762aa2785259addae241b
Stefan Wienert <stefan.wienert@pludoni.de>
2014-08-30 13:08:36 +0200

    can update own goal

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,10 +10,16 @@ class Ability
     else
       challenges = Challenge.upcoming_or_running.visible
       can :read, Challenge, id: Challenge.visible.pluck(:id)
+
+      active_participations = Participation.where(challenge_id: Challenge.running).where(user_id: user.id)
       can :create, Participation, challenge_id: challenges.pluck(:id)
-      can :manage, Participation, user_id: user.id
-      can :create, ActivityLog, participation_id: Participation.where(challenge_id: Challenge.running, user_id: user.id).pluck(:id)
-      can :manage, Participation, user_id: user.id
+      can :create, ActivityLog, participation_id: active_participations.pluck(:id)
+
+      can :destroy, Participation, user_id: user.id
+      can :update, Participation, active_participations do |p|
+        p.created_at > 7.days.ago
+      end
+
       cannot [:edit, :update], ActivityLog do |log|
         log.created_at < 1.day.ago
       end
