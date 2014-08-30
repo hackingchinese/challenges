ae4f037fcb7db10f80f3457fdf1e5f73f5426da9
Stefan Wienert <stefan.wienert@pludoni.de>
2014-08-30 12:40:37 +0200

    added haiku option as unit; fixed links in mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.log_level = :info
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host =>  'challenge.hackingchinese.com'}
+  config.action_mailer.default_url_options = { :host =>  'challenges.hackingchinese.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
