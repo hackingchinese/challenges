c6b8950b0634a2c9afbaa4f3d865f16169846e7c
Stefan Wienert <stefan.wienert@pludoni.de>
2016-06-19 10:50:10 +0800

    Test: clear mails after each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,6 +32,7 @@ RSpec.configure do |config|
     page.driver.browser.url_blacklist = ["http://fonts.googleapis.com",  "http://fonts.useso.com"]
   end
   config.before(:each) do |example|
+    ActionMailer::Base.deliveries.clear
     ex = defined?(example.example) ? example.example : example
     DatabaseCleaner.strategy = ex.metadata[:js] ? :deletion : :transaction
     DatabaseCleaner.start
